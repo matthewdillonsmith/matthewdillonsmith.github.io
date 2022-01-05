3ba8a87ef022b2f03546f8cf27ea3dd4b3ff5306
smittymx88 <matthew.dillon.smith@gmail.com>
2022-01-05 07:58:20 -0500

    updated site

---
+++ assets/apps-script.js
@@ -0,0 +1,9 @@
+function changetexttest1(id) {
+    return document.getElementById(id).innerHTML=
+    "It worked!";
+}
+
+function changetexttest2(id) {
+    return document.getElementById(id).innerHTML=
+    "This is a test for my external JavaScript";
+}
