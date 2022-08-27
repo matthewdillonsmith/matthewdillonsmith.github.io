fd48bf0207410266c61fbfed7350748f4a9982c3
Matthew Dillon Smith <matthew.dillon.smith@outlook.com>
2022-08-27 01:25:57 -0400

    formatting changes
    created a separate page for "Timeline"

--- assets/apps-script.js
+++ assets/apps-script.js
@@ -6,26 +6,28 @@ function changetexttest2(id) {
     return document.getElementById(id).innerHTML="This is a test for my external JavaScript";
 }
 
+// Script for the 'introduction' set of cards
 function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent1, tablinks1;
+  tabcontent1 = document.getElementsByClassName("tabcontent-1");
+  for (i = 0; i < tabcontent1.length; i++) {
+    tabcontent1[i].style.display = "none";
   }
-  
+
+  // Remove the background color of all tablinks/buttons
+  tablinkouter = document.getElementsByClassName("tablink-outer");
+  for (i = 0; i < tablinkouter.length; i++) {
+    tablinkouter[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+
